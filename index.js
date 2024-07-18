e088c6863bea7244528afae94f8b96af2264e0c7
exa31 <bloodsuker18@gmail.com>
2024-07-19 01:18:55 +0700

    test deploy

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const route = require('./routes/routes');
 
 app.use(route)
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Menentukan port dari variabel lingkungan atau default ke 3000
+const PORT = process.env.PORT || 3000;
+// Menentukan host dari variabel lingkungan atau default ke '0.0.0.0' untuk deployment
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
