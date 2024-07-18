95213e05e577f15b06d056290c648f4c5c09f8a7
exa31 <bloodsuker18@gmail.com>
2024-07-19 06:05:36 +0700

    add vercel.json

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const route = require('./routes/routes');
 
 app.use(route)
 
-// Menentukan port dari variabel lingkungan atau default ke 3000
-const PORT = process.env.PORT || 3000;
-// Menentukan host dari variabel lingkungan atau default ke '0.0.0.0' untuk deployment
-const HOST = process.env.HOST || '0.0.0.0';
-
-app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
 });
